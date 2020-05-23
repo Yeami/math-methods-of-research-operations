10b5b7f2d64012953663a4e1b8da438560b3ec56
Yeami <ivanov.nikita@yeami.group>
2020-05-24 00:44:43 +0300

    Implement the Utils class with abilities to print the result and to round number to epsilon

---
+++ Utils.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace UnimodalFunctions
+{
+    public static class Utils
+    {
+        public static double RoundToEps(double epsilon, double number)
+        {
+            return Math.Round(number * Math.Pow(epsilon, -1)) / Math.Pow(epsilon, -1);
+        }
+
+        public static void Print
+        (
+            double epsilon,
+            double point,
+            double function,
+            int iterationAmount,
+            int functionCallAmount
+        )
+        {
+            Console.WriteLine(
+                "\n----- Dichotomy method -----" +
+                $"\nEpsilon: {epsilon}" +
+                $"\nx*: {point}" +
+                $"\nf*: {function}" +
+                $"\nIteration amount: {iterationAmount}" +
+                $"\nFunction calls amount: {functionCallAmount}"
+            );
+        }
+    }
+}
