2a9b502fa95b7fadc93e517f42b56351eca7556d
Yeami <ivanov.nikita@yeami.group>
2020-05-24 00:47:25 +0300

    Implement the Dichotomy method and add the start data to the Program class

---
+++ DichotomyMethod.cs
@@ -0,0 +1,49 @@
+namespace UnimodalFunctions
+{
+    public class DichotomyMethod
+    {
+        private double _a;
+        private double _b;
+        private readonly double _eps;
+        private int _iterationAmount;
+        private readonly Function _function;
+
+        public DichotomyMethod(int a, int b, double eps)
+        {
+            _a = a;
+            _b = b;
+            _eps = eps;
+            _function = new Function();
+        }
+
+        public void Solve()
+        {
+            var delta = _eps / 3;
+
+            do
+            {
+                var x1 = (_a + _b - delta) / 2;
+                var x2 = (_a + _b + delta) / 2;
+
+                var firstFunction = _function.Solve(x1);
+                var secondFunction = _function.Solve(x2);
+
+                if (firstFunction <= secondFunction)
+                {
+                    _b = x2;
+                }
+                else
+                {
+                    _a = x1;
+                }
+
+                _iterationAmount++;
+            } while (!(_b - _a < _eps));
+
+            var point = Utils.RoundToEps(_eps, (_a + _b) / 2);
+            var function = Utils.RoundToEps(_eps, _function.Solve(point));
+
+            Utils.Print(_eps, point, function, _iterationAmount, _function.CallAmount);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
-
-namespace UnimodalFunctions
+﻿namespace UnimodalFunctions
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private const int A = 0;
+        private const int B = 2;
+        private static readonly double[] Epsilons = {0.01, 0.0001, 0.00000001};
+
+        private static void Main()
         {
+            foreach (var epsilon in Epsilons)
+            {
+                new DichotomyMethod(A, B, epsilon).Solve();
+            }
         }
     }
 }
