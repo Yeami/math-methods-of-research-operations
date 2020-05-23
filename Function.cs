9c76f65885d01e58a710b574d944e09520f32a0a
Yeami <ivanov.nikita@yeami.group>
2020-05-24 00:46:20 +0300

    Improve the Function class to count the number of function calls

--- Function.cs
+++ Function.cs
@@ -2,10 +2,18 @@ using System;
 
 namespace UnimodalFunctions
 {
-    public static class Function
+    public class Function
     {
-        public static double Solve(double x)
+        public int CallAmount { get; private set; }
+
+        public Function()
         {
+            CallAmount = 0;
+        }
+
+        public double Solve(double x)
+        {
+            CallAmount += 1;
             return 4 * Math.Pow(3 - x, 2.0 / 3.0) + 2 * Math.Pow(x, 3.0);
         }
     }
